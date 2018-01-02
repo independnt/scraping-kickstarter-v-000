04814051c3fcbb70faee85b4e013e5604f49c2de
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2018-01-02 18:21:19 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,6 +3,10 @@ require 'pry'
 
 #projects: kickstarter.css("li.project.grid_4")
 #title: project.css("li.project.grid_4")
+#image link: project.css("div.project-thumbnail a img").attribute("src").value
+#description: project.css("p.bbcard_blurb").text
+#location: project.css("ul.project-meta span.location-name").text
+#percent funded:project.css("ul.project-stats li.first.funded strong").text
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
